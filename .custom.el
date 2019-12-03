7a05ea6183669cc541d08d52178bc8652ab6c8ed
alejnd <alemail@gmail.com>
2019-12-03 21:11:39 +0100

    disable custom hl

--- .custom.el
+++ .custom.el
@@ -3,13 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a22f40b63f9bc0a69ebc8ba4fbc6b452a4e3f84b80590ba0a92b4ff599e53ad0" "70ed3a0f434c63206a23012d9cdfbe6c6d4bb4685ad64154f37f3c15c10f3b90" "7dc3fe8fadb914563790a3fbe587fd455626442f66da333ea4de2c455feefb98" default)))
  '(package-selected-packages
    (quote
-    (monokai-pro-theme drag-stuff shell-pop tabbar powerline markdown-mode which-key wgrep use-package undo-tree smex smartparens pyenv-mode py-autopep8 pip-requirements org-projectile org-bullets neotree multiple-cursors magit-popup magit intero hlinum go-eldoc ggtags flymake-go expand-region exec-path-from-shell elpy doom-themes dashboard counsel-projectile company-go company-c-headers avy)))
+    (airline-themes ztree which-key wgrep use-package undo-tree tabbar smex smartparens shell-pop pyenv-mode py-autopep8 powerline pip-requirements org-projectile org-bullets neotree multiple-cursors monokai-pro-theme markdown-mode magit-popup magit intero hlinum gruvbox-theme go-eldoc ggtags flymake-go expand-region exec-path-from-shell elpy drag-stuff doom-themes dashboard counsel-projectile company-go company-c-headers avy)))
  '(shell-pop-full-span t)
  '(shell-pop-shell-type
    (quote
-    ("ansi-term" "*ansi-term*"
+    ("ansi-term" "*term*"
      (lambda nil
        (eshell shell-pop-term-shell)))))
  '(shell-pop-term-shell "/bin/bash")
